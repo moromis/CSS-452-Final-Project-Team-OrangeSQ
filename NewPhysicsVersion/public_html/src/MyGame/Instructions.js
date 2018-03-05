ee2bde5f02a7900d97d0b462d7c7835f66ba79ef
SharanyaSudhakar <32864742+SharanyaSudhakar@users.noreply.github.com>
2018-03-05 15:14:51 -0800

    instructions added

--- NewPhysicsVersion/public_html/src/MyGame/Instructions.js
+++ NewPhysicsVersion/public_html/src/MyGame/Instructions.js
@@ -5,66 +5,61 @@
 
 /*jslint node: true, vars: true, white: true */
 /*global gEngine, Scene, GameObjectset, TextureObject, Camera, vec2,
- Renderable, TextureRenderable, FontRenderable, SpriteRenderable, LightRenderable, IllumRenderable,
- GameObject, TiledGameObject, ParallaxGameObject, Hero, Minion, Dye, Light, BlockManager, HelperFunctions */
+  Renderable, TextureRenderable, FontRenderable, SpriteRenderable, LightRenderable, IllumRenderable,
+  GameObject, TiledGameObject, ParallaxGameObject, Hero, Minion, Dye, Light, BlockManager, HelperFunctions */
 /* find out more about jslint: http://www.jslint.com/help.html */
 
 "use strict";  // Operate in Strict mode such that variables must be declared before used!
 
 "use strict";  // Operate in Strict mode such that variables must be declared before used!
 
-function StartScreen() {
-    this.kBG = "assets/SplashScreen.png";
+function Instructions() {
+    this.kBG = "assets/instructions.png";
     this.BGWidth = 1024;
     this.initialLightLevel = 3.5;
     this.CameraCanvasWidth = HelperFunctions.Core.getCameraWidth();
     this.CameraCenter = HelperFunctions.Core.getCameraCenter();
     this.CanvasWidth = HelperFunctions.Core.getCanvasWidth();
     this.CanvasHeight = HelperFunctions.Core.getCanvasHeight();
-
+    
     // The camera to view the scene
     this.mCamera = null;
     this.mStatusMsg = null;
-    this.showInstructions = false;
 }
-gEngine.Core.inheritPrototype(StartScreen, Scene);
+gEngine.Core.inheritPrototype(Instructions, Scene);
 
-StartScreen.prototype.loadScene = function () {
-    gEngine.Textures.loadTexture(this.kBG);
+Instructions.prototype.loadScene = function () {
+        gEngine.Textures.loadTexture(this.kBG);
 };
 
-StartScreen.prototype.unloadScene = function () {
+Instructions.prototype.unloadScene = function () {
     gEngine.LayerManager.cleanUp();
     gEngine.Textures.unloadTexture(this.kBG);
-
-    var nextLevel = null;
-    if (this.showInstructions)
-        nextLevel = new Instructions();
-    else
-        nextLevel = new MyGame();  // load the next level
+    
+    var nextLevel = new MyGame();  // load the next level
     gEngine.Core.startScene(nextLevel);
 };
 
-StartScreen.prototype.initialize = function () {
+Instructions.prototype.initialize = function () {
     this.mCamera = new Camera(
-            vec2.fromValues(this.CameraCenter, this.CameraCenter), // position of the camera
-            this.CameraCanvasWidth, // width of camera
-            [0, 0, this.CanvasWidth, this.CanvasWidth]              // viewport (orgX, orgY, width, height)
-            );
-    this.mCamera.setBackgroundColor([0.8, 0.8, 0.8, 1]);
-
+        vec2.fromValues(this.CameraCenter, this.CameraCenter),  // position of the camera
+        this.CameraCanvasWidth,                                 // width of camera
+        [0, 0, this.CanvasWidth, this.CanvasWidth]              // viewport (orgX, orgY, width, height)
+    );
+    this.mCamera.setBackgroundColor([0.5, 1, 1, 1]);
+    
     //intialize background
     var bgR = new LightRenderable(this.kBG);
-    bgR.setElementPixelPositions(0, this.CameraCanvasWidth, 0, this.CameraCanvasWidth - 200);
-    bgR.getXform().setSize(this.BGWidth, this.BGWidth);
-    bgR.getXform().setPosition(this.CameraCenter, this.CameraCenter);
+    bgR.setElementPixelPositions(0, 1024, 0, 512);
+    bgR.getXform().setSize(1300, 640);
+    bgR.getXform().setPosition(635, 320);
     this.mBG = new GameObject(bgR);
-
+    
     this.mStatusMsg = new FontRenderable("");
-    this.mStatusMsg.setColor([1, 1, 1, 1]);
-    this.mStatusMsg.getXform().setPosition(70, 40);
-    this.mStatusMsg.setTextHeight(32);
-    this.mStatusMsg.setText("P - Requirements    SPACEBAR - Start Game...");
+    this.mStatusMsg.setColor([0.9, 0.2, 0.3, 1]);
+    this.mStatusMsg.getXform().setPosition(this.CanvasWidth / 2 - 90, 40);
+    this.mStatusMsg.setTextHeight(24);
+    this.mStatusMsg.setText("SPACEBAR - start game...");
     gEngine.LayerManager.addToLayer(gEngine.eLayer.eHUD, this.mStatusMsg);
     gEngine.LayerManager.addToLayer(gEngine.eLayer.eBackground, this.mBG);
     gEngine.DefaultResources.setGlobalAmbientIntensity(this.initialLightLevel);
@@ -72,7 +67,7 @@ StartScreen.prototype.initialize = function () {
 
 // This is the draw function, make sure to setup proper drawing environment, and more
 // importantly, make sure to _NOT_ change any state.
-StartScreen.prototype.draw = function () {
+Instructions.prototype.draw = function () {
     // Step A: clear the canvas
     gEngine.Core.clearCanvas([1, 1, 1, 1.0]); // clear to light gray
 
@@ -82,13 +77,8 @@ StartScreen.prototype.draw = function () {
 
 // The update function, updates the application state. Make sure to _NOT_ draw
 // anything from this function!
-StartScreen.prototype.update = function () {
-    if (gEngine.Input.isKeyClicked(gEngine.Input.keys.Space)) {
-        gEngine.GameLoop.stop();
-    }
-
-    if (gEngine.Input.isKeyClicked(gEngine.Input.keys.P)) {
-        this.showInstructions = true;
-        gEngine.GameLoop.stop();
-    }
+Instructions.prototype.update = function () {
+      if(gEngine.Input.isKeyClicked(gEngine.Input.keys.Space)){
+                gEngine.GameLoop.stop();
+            }
 };
